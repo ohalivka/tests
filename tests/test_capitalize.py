0cccfdaf406706a682fa7262cf73376451b98f33
Alina <alivka@MacBook-Air-Alina.local>
2023-09-06 22:45:39 +0300

    refactor

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,9 +1,4 @@
-from package.capitalize import capitalize
+from capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
-
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
-
-print('Все тесты пройдены!')
+assert capitalize('hello') == 'Hello'
+assert capitalize('') == ''
